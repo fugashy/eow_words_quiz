75d7d5fcfde6c47a67d9c14e424ebdf5902a5af1
fugashy <fugashy@icloud.com>
2020-06-13 20:09:11 +0900

    Extract main sequence as a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,31 @@
-pub mod csv_random_picker;
-pub mod translate_shell_client;
+mod csv_random_picker;
+mod translate_shell_client;
+
+use std::io;
+
+use csv_random_picker::CsvRandomPicker;
+use translate_shell_client::TranslateShellClient;
+
+pub fn run() {
+    let file_path = String::from("/tmp/words.tsv");
+    let picker = CsvRandomPicker::new(&file_path).unwrap();
+
+    let some_row = picker.random_pickup().unwrap();
+    println!(
+        "Do you remember what '{}' means that you searched at {}?\nPress enter if you want answers.",
+        some_row.word, some_row.date
+    );
+
+    let mut some_input = String::new();
+    io::stdin().read_line(&mut some_input).ok(); // Errでもなんでも構わない
+
+    let mut translator = TranslateShellClient::new().unwrap();
+    match translator.trans(&some_row.word) {
+        Ok(v) => println!(
+            "{}\n{}",
+            v.0.unwrap_or("".to_string()),
+            v.1.unwrap_or("".to_string())
+        ),
+        Err(e) => println!("{}", e),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
 extern crate eow_words_quiz;
-use eow_words_quiz::{
-    csv_random_picker::CsvRandomPicker, translate_shell_client::TranslateShellClient,
-};
 
 fn main() {
-    let file_path = String::from("/tmp/words.tsv");
-    let picker = CsvRandomPicker::new(&file_path).unwrap();
-    let some_row = picker.random_pickup().unwrap();
-    println!(
-        "Do you remember what '{}' means that you searched at {}",
-        some_row.word, some_row.date
-    );
-
-    let mut translator = TranslateShellClient::new().unwrap();
-    match translator.trans(&some_row.word) {
-        Ok(v) => println!(
-            "{}\n{}",
-            v.0.unwrap_or("".to_string()),
-            v.1.unwrap_or("".to_string())
-        ),
-        Err(e) => println!("{}", e),
-    }
+    eow_words_quiz::run();
 }
