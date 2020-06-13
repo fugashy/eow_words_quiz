7f1c8c766bcacc61762343a76f0ef6176ce951e1
fugashy <fugashy@icloud.com>
2020-06-13 20:23:41 +0900

    Modify to use repeatedly
    Note: Generation of a translator is redundant.

--- src/lib.rs
+++ src/lib.rs
@@ -8,26 +8,31 @@ use csv_random_picker::CsvRandomPicker;
 use translate_shell_client::TranslateShellClient;
 
 pub fn run(mut args: env::Args) {
+    // 最初はファイル名なので流す
     args.next();
+
     let file_path = String::from(args.next().unwrap());
     let picker = CsvRandomPicker::new(&file_path).unwrap();
 
-    let some_row = picker.random_pickup().unwrap();
-    println!(
+    loop {
+        let some_row = picker.random_pickup().unwrap();
+        println!(
         "Do you remember what '{}' means that you searched at {}?\nPress enter if you want answers.",
-        some_row.word, some_row.date
-    );
+        some_row.word, some_row.date);
 
-    let mut some_input = String::new();
-    io::stdin().read_line(&mut some_input).ok(); // Errでもなんでも構わない
+        let mut some_input = String::new();
+        io::stdin().read_line(&mut some_input).ok(); // Errでもなんでも構わない
 
-    let mut translator = TranslateShellClient::new().unwrap();
-    match translator.trans(&some_row.word) {
-        Ok(v) => println!(
-            "{}\n{}",
-            v.0.unwrap_or("".to_string()),
-            v.1.unwrap_or("".to_string())
-        ),
-        Err(e) => println!("{}", e),
+        // TODO(fugashy)
+        // ここで毎回生成する必要はないのだが，Subprocessの使い方がイケてないのかエラーになってしまう
+        let mut translator = TranslateShellClient::new().unwrap();
+        match translator.trans(&some_row.word) {
+            Ok(v) => println!(
+                "{}\n{}",
+                v.0.unwrap_or("".to_string()),
+                v.1.unwrap_or("".to_string())
+            ),
+            Err(e) => println!("{}", e),
+        }
     }
 }
